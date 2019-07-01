9b39b4b950115ea7256035d9d6283b9dea676d8d
Juan Galeazzi <jgaleazzi@gmail.com>
2019-07-01 20:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var beatles = [];
   for (var i=0; i<musicians.length;i++) {
     var bandMember = musicians[i] + " plays " + instruments[i];
-    beatles.push[bandMember];
+    beatles.push(bandMember);
   }
 
   return beatles;
