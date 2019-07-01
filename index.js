ee8820892206318e5208a4a36775d947eaa4c4f0
Juan Galeazzi <jgaleazzi@gmail.com>
2019-07-01 16:38:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i=0; i<musicians.length;i++) {
+    var bandMember = musicians[i] + " plays " + instruments[i];
+    beatles.push[bandMember];
+  }
+
+  return beatles;
+}
